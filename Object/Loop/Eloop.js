a194db22749aaa3b79b8398bb9ae6a4b72cd9e25
Sachinmeena007 <sachinmeena0287@gmail.com>
2025-05-21 19:20:52 +0530

    update

---
+++ Object/Loop/Eloop.js
@@ -0,0 +1,14 @@
+// let arr=[1,2,3,4,5]
+// for(let i of arr){
+
+//     console.log(i)
+// }
+
+let num=[1,2,3,4]
+
+ let arr= num.map((e)=>{
+ return e*e;
+ 
+
+})
+console.log(arr)
--- Object/object.js
+++ Object/object.js
@@ -1,12 +1,23 @@
-let details={
+// let details={
 
-    name:"sachin",age:21,city:"bhopal",
-    fruites:["mango","apple,","papaya","banana"]
+//     name:"sachin",age:21,city:"bhopal",
+//     fruites:["mango","apple,","papaya","banana"]
     
+// }
+// console.log(details.city) // object acces
+// details.college="lnct"; //add data
+// delete details.age   // delete
+// details.age=26;  //update
+// console.log(details)
+// console.log(details.fruites[3])
+
+//object ka name currlye barsces ke anadar kye and value ke pair main kye uniqe honi chaiye
+
+let welcome={
+    name:"sachin",
+    city:"bhopal",
+    age:21
 }
-console.log(details.city) // object acces
-details.college="lnct"; //add data
-delete details.age   // delete
-details.age=26;  //update
-console.log(details)
-console.log(details.fruites[3])
+for(let i in welcome){
+    console.log(i+ "=> "+welcome[i])
+}
