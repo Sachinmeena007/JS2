d8ed4f2b03b291495cbc5a5f9a39fd910eafd51c
Sachinmeena007 <sachinmeena0287@gmail.com>
2025-05-22 14:20:53 +0530

    update

--- Object/Loop/Eloop.js
+++ Object/Loop/Eloop.js
@@ -4,11 +4,27 @@
 //     console.log(i)
 // }
 
-let num=[1,2,3,4]
 
- let arr= num.map((e)=>{
- return e*e;
+// MAP LOOP, FILTER LOOP
+// let num=[1,2,3,4]
+
+//  //let arr= num.map((e)=>{
+
+//     let arr= num.filter((e)=>{
+//  return e%2==0;
  
 
-})
-console.log(arr)
+// })
+// console.log(arr)
+//   let a= 5=="5"
+// console.log(a)
+
+let welcome={
+
+  Name:"Sachin",
+  Age: 21,
+  City:"Bhopal"
+}
+for(let i in welcome){
+  console.log(i+ "=> "+welcome[i])
+}
