c2c4dde0a56418cd690dca774f209ef1e85471c6
Sachinmeena007 <sachinmeena0287@gmail.com>
2025-05-23 12:43:28 +0530

    update

---
+++ Object/Loop/DOM/dom.js
@@ -0,0 +1,37 @@
+// let btn=()=>{
+
+// let a=document.getElementById("text")
+// a.innerHTML="Sachin"
+// let c=document.("text2")
+// c.innerHTML="prem bhai"
+
+
+// }
+
+// let btn2=()=>{
+
+// b=document.querySelector(".sm")
+// b.innerHTML="jay ram ji kee"
+// b.innerHTML.style.color="red",
+
+
+// }
+
+
+let c1=()=>{
+
+let a=document.querySelector("#sm")
+a.style.color="red"
+
+}
+let c2=()=>{
+
+
+    let b=document.querySelector("#sm1")
+    b.style.color="blue";
+}
+
+let c3=()=>{
+    c=document.querySelector("#sm2")
+    c.style.color="green";
+}
