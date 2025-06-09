f44e6b79d995471df7c7c34a51efc24a0d8a9e51
Sachinmeena007 <sachinmeena0287@gmail.com>
2025-06-09 19:26:05 +0530

    update

--- Object/Loop/DOM/FormValid/Form.js
+++ Object/Loop/DOM/FormValid/Form.js
@@ -1,5 +1,10 @@
-let c4=()=>{
-    d=document.querySelector("#box")
-    d.style.backgroundColor=" "
-    
- }
+let show=()=>{
+
+let text=document.querySelector("#text").value
+let text2=document.querySelector("#text2").value
+let num=parseInt(text)+parseInt(text2)
+let sm=document.querySelector("#sm")
+sm.innerHTML=num
+
+
+}
